aa20ecbac8c5e27439405c85eeae7a0493f31a2d
Kopi <kodewithkopi@gmail.com>
2025-11-09 20:14:32 +0530

    Add files via upload

---
+++ Precedence & Associativity/Main.java
@@ -0,0 +1,43 @@
+public class Main
+{
+    public static void main(String[] args)
+    {
+        System.out.println("\nPrecedence & Associativity\n");
+        int a = 13 + 2 * 9 - 8 / 4 ;
+        /*
+         EVALUATION:
+          a = 13 + 2 * 9 - 8 / 4 
+          a = 13 + 18 - 8 / 4 
+          a = 13 + 18 - 2
+          a = 31 - 2
+          a = 29
+         */
+        System.out.println("Q1] 13 + 2 * 9 - 8 / 4 = " + a);
+
+
+        int b = (13 + 2) * (9 - 8) / 4 ;
+        /*
+         EVALUATION:
+          b = (13 + 2) * (9 - 8) / 4
+          b = 15 * (9 - 8) / 4
+          b = 15 * 1 / 4
+          b = 15 / 4
+          b = 3
+         */
+        System.out.println("Q2] (13 + 2) * (9 - 8) / 4  = " + b);
+
+        boolean c = ! ((3 >= 1) || 4 + 9 != 8 - 3) ;
+        /*
+         EVALUATION:
+          c = ! ((3 >= 1) || 4 + 9 != 8 - 3)
+          c = ! (true || 4 + 9 != 8 - 3)
+          c = ! (true || 13 != 8 - 3)
+          c = ! (true || 13 != 5)
+          c = ! (true || true)
+          c = ! (true)
+          c = false
+         */
+        System.out.println("Q3] ! ((3 >= 1) || 4 + 9 != 8 - 3) = "  + c);
+
+    }
+}
